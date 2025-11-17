6777883c797f6d0d913a0036594eb7200c8566f7
Matteo <45847997+sanriodev@users.noreply.github.com>
2025-11-17 07:32:26 +0100

    fix: use master

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -7,10 +7,7 @@ set -e
 cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
 
 # Install Flutter using git.
-git clone https://github.com/flutter/flutter.git $HOME/flutter
-cd $HOME/flutter
-git fetch
-git checkout 3.35.3
+git clone https://github.com/flutter/flutter.git --depth 1 -b master $HOME/flutter
 export PATH="$PATH:$HOME/flutter/bin"
 
 # Install Flutter artifacts for iOS (--ios), or macOS (--macos) platforms.
